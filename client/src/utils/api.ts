fde668e232bf8a933ab18f363ccf1e6f338c4545
ioproxxy <ioproxxy@gmail.com>
2025-12-14 17:53:07 +0300

    feat: Add Axios API client with a token-based authorization interceptor.

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api', // Hardcoded local for MVP
+    baseURL: '/api', // Use relative path to leverage Vite proxy
 });
 
 api.interceptors.request.use((config) => {
